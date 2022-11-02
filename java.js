75089b64cc925dc2147d45437829bc113f7b1716
Iuli4 <yulya.popa.962005@gmail.com>
2022-11-02 20:35:04 +0200

    cicle i javaScript

---
+++ java.js
@@ -0,0 +1,73 @@
+// const arr = [];
+// const arr1 = [1,2,"pirivet",3,4];
+// console.log(arr)
+// //console.log(arr1[2])
+// console.log(arr1.length) //сколько элементов
+// const user=prompt("Как тебя зовут?")
+// const user1=prompt("Сколько тебе лет?")
+
+// const userArray =[]
+// const userArray1=["Julia"]
+// userArray.push(userArray)
+// userArray.push(user)
+// userArray.push(user1)
+// console.log(userArray)
+// let obj={
+//     name:"Julia",
+//     age : "17"  ,
+//     gen : "W"   ,
+// }
+// console.log(obj)
+// console.log(obj.name,obj.age,obj.gen)
+
+// let obj_1 ={
+//     name:"Nina",
+//     age:"39",
+//     gen:"F",
+//      namechild:{
+//         name:"Viorel",
+//         age:"5",
+//         gen:"M"
+//      }
+// }
+// console.log(obj_1.namechild.age)
+// const user =[
+//     { name :"Alex"},
+//     { name: "Ion"}
+// ]
+// user.forEach(el=>{
+//     console.log(el.name);
+// })
+  const array = [
+     "1",
+     "2",
+     "3",
+     "4",
+     "5",
+    "6",
+ ];
+// array.forEach(el =>{
+//     if (el.includes("3")){
+//         console.log(el);
+//     }else{
+//         console.log("No");
+//     }
+//     // console.log(el.includes("6"))
+// })
+// function name(array) {
+//     parrams.forEach(element =>{
+//         console.log(element);
+//     });
+    
+// }
+function name(){
+    console.log(array)
+    }
+    function name1 (params,inde){
+        let nul= params + inde
+        console.log(nul);
+    }
+    name1(array)
+    name1("saliut")
+    name1(10,10)
+    
